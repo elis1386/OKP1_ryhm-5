eea6b7ec2efddbb978468f2abe6cbd7f99cb50b2
elis1386 <s2200704@BC-C02J64A8Q6LR-MacBook-Air.local>
2022-12-09 08:18:30 +0200

    modify fooed screen

--- cartScreen/cart.js
+++ cartScreen/cart.js
@@ -1,8 +1,7 @@
-const pathToProductsImages = 'gallery';
+const pathToProductsImages = "gallery";
 const addBtn = document.querySelector("#addToCart");
 const cartEl = document.querySelector("#cart-items");
-
-
+const orderEL = document.querySelector("#order");
 
 function renderNewProductToCart(product) {
   return `
@@ -28,7 +27,7 @@ function insertProductsIntoCart() {
   const productId = urlParams.get("id");
   for (let product of products) {
     if (product.id == productId) {
-        console.log(product,productId)
+      console.log(product, productId);
       cartEl.insertAdjacentHTML("afterbegin", renderNewProductToCart(product));
       break;
     }
@@ -36,12 +35,43 @@ function insertProductsIntoCart() {
 }
 insertProductsIntoCart();
 
-
-function showConfirmModal()
-{
-  const confirmModal = document.querySelector('#confirm')
-  confirmModal.showModal()
+function renderOrder(product) {
+  return `
+   <article class="order_info_product">
+            <figure class="product_photo">
+              <img src="${pathToProductsImages}/${product.image}" alt="" />
+            </figure>
+            <figcaption class="product_text">
+              <h3>${product.name}</h3>
+              <article class="stars">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <p>${product.price}€</p>
+              </article>
+            </figcaption>
+          </article> 
+  
+    `;
 }
-   
- 
 
+function insertOrder() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = urlParams.get("id");
+  for (let product of products) {
+    if (product.id == productId) {
+      console.log(product, productId);
+      orderEL.insertAdjacentHTML("afterend", renderOrder(product));
+      break;
+    }
+  }
+}
+
+
+function showConfirmModal() {
+  const confirmModal = document.querySelector("#confirm");
+  confirmModal.showModal();
+  insertOrder();
+}
